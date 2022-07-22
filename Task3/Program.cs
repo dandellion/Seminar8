c5daf500159943a5d8ff0f76e306ff873ba4adb4
Tasa <greatwillow@mail.ru>
2022-07-23 01:13:09 +0300

    third task

---
+++ Task3/Program.cs
@@ -0,0 +1,49 @@
+﻿int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = randomizer.Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int [,] Multiplication (int[,] matrix1, int[,] matrix2)
+{
+    int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+     for (int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            result [i,j] = matrix1 [i,j]*matrix2 [i,j];
+        }
+    }
+    return result;
+}
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+int[,] matrix1 = InitMatrix(m, n);
+int[,] matrix2 = InitMatrix(m, n);
+PrintMatrix(matrix1);
+Console.WriteLine();
+PrintMatrix(matrix2);
+Console.WriteLine();
+int[,] matrixresult = Multiplication(matrix1,matrix2);
+PrintMatrix(matrixresult);
