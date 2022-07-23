b2177a916d1082a1d76649f07444a331e81284ee
Tasa <greatwillow@mail.ru>
2022-07-23 23:48:02 +0300

    forth task

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿int [,,] InitMatrix(int n)
+{
+    int[,,] matrix = new int[n, n, n];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                matrix[i, j, k] = randomizer.Next(1, 10)*10+randomizer.Next(1, 10);
+            }
+           
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,,] matrix, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            for (int k = 0; k < n; k++)
+            {
+                Console.Write($"{matrix[i, j, k]}({i}, {j}, {k})  ");
+            }
+            
+        }
+        Console.WriteLine();
+    }
+}
+int n = 2;
+int[,,] matrix = InitMatrix(n);
+PrintMatrix(matrix, n);
